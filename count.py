c90793ca4ca144c5b8fbcc234208b4752b801c15
Samhitha <guntisamhitha16@gmail.com>
2025-12-02 11:05:23 +0530

    initial

---
+++ count.py
@@ -0,0 +1,14 @@
+
+def count_lines(path):
+    with open(path, 'r', encoding='utf-8', errors='replace') as f:
+        return sum(1 for _ in f)
+
+if __name__ == "__main__":
+    path = input("Enter path to text file: ").strip()
+    if not path:
+        print("No file specified.")
+    else:
+        
+        n = count_lines(path)
+        print(f"Number of lines: {n}")
+        
